3689d201ba085b57591aa150083308002a45e8d0
Trifle22 <xromchenkov99@gmail.com>
2020-11-12 00:01:04 +0300

    logOut

--- js/main.js
+++ js/main.js
@@ -7,12 +7,20 @@ menuToggle.addEventListener('click', function (event) {
   menu.classList.toggle('visible');
 });
 
+const regExpEmail = /^\w+@\w+\.\w{2,}$/;
+
 
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
 const passwordInput = document.querySelector('.login-password');
 const loginSignUp = document.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+const exitElem = document.querySelector('.exit');
+const editELem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
 
 const listUsers = [
   {
@@ -31,23 +39,58 @@ const listUsers = [
 
 const setUsers = {
   user: null,
-  logIn(email, password) {
+  logIn(email, password, handler) {
+    if (!regExpEmail.test(email)) return alert('Введите корректный e-mail')
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('такого пользователя не существует');
+    }
+  },
+  logOut(handler) {
+    this.user = null;
+    handler();
+  },
+  signUp(email, password, handler) {
+    if (!regExpEmail.test(email)) return alert('Введите корректный e-mail');
+    if (!email.trim() || !password.trim()) {
+      alert('Введите данные');
+      return;
+    }
 
-  },
-  logOut() {
-    console.log("logOut");
-  },
-  signUp(email, password) {
     if (!this.getUser(email)) {
-      listUsers.push({email, password, displayName: email})
+      const user = {email, password, displayName: email};
+      listUsers.push(user)
+      this.authorizedUser(user);
+      handler();
     } else {
       alert('такой пользователь существует')
     }
   },
-  getUser() {
-    console.log(123);
+
+  getUser(email) {
+    return listUsers.find(item => item.email === email);
+  },
+  authorizedUser(user) {
+    this.user = user;
   }
 };
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+}
+
 
 loginForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -55,7 +98,8 @@ loginForm.addEventListener('submit', event => {
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
 
-  setUsers.logIn(emailValue, passwordValue);
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+  loginForm.reset();
 });
 
 loginSignUp.addEventListener('click', event => {
@@ -64,7 +108,19 @@ loginSignUp.addEventListener('click', event => {
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
 
-  setUsers.signUp(emailValue, passwordValue);
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+  loginForm.reset();
 });
 
+exitElem.addEventListener('click', event => {
+  event.preventDefault();
+  setUsers.logOut(toggleAuthDom);
 
+});
+
+editELem.addEventListener('click', event => {
+  event.preventDefault();
+  editContainer.classList.toggle('visible');
+});
+
+toggleAuthDom();
